48666abfb10eac02dcad9fd6003b570c8c8c1001
WangAphisit Nattasan <ts-wangaphi.nattasan@rakuten.com>
2023-10-04 00:11:25 +0900

    make Timeline components rendering dynmically

--- src/lib/components/TimelineItem.svelte
+++ src/lib/components/TimelineItem.svelte
@@ -4,28 +4,33 @@
 	export let title: string = '(Title)';
 	export let timestamp: string = '(timestamp)';
 	export let description: string = '(Description)';
-	export let badgeText: string = '(Badge)';
+	export let badgeText: string = '';
+	export let icon: string | null = '';
 </script>
 
 <li class="mb-10 ml-6">
 	<span
-		class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+		class="absolute flex items-center justify-center w-8 h-8 bg-secondary-100 rounded-full -left-4 ring-8 ring-white dark:ring-gray-900 dark:bg-secondary-900"
 	>
-		<Icon icon="tdesign:calendar" />
+		{#if !icon}
+			<Icon icon="tdesign:calendar" />
+		{:else}
+			<Icon {icon} />
+		{/if}
 	</span>
-	<h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+	<h3 class="flex items-center mb-2 text-lg font-semibold text-primary-700 dark:text-primary-200">
 		{title}
 		{#if badgeText}
 			<span
 				class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3"
-				>{badgeText}</span
+				>{badgeText.toUpperCase()}</span
 			>
 		{/if}
 	</h3>
-	<time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+	<time class="block mb-2 text-sm font-normal leading-none text-surface-400 dark:text-surface-500"
 		>on {timestamp}</time
 	>
-	<p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+	<p class="mb-4 text-base font-normal text-secondary-500 dark:text-secondary-400">
 		{description}
 	</p>
 	<!-- Slot: Default -->
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,6 +2,9 @@
 	import { Avatar } from '@skeletonlabs/skeleton';
 	import Timeline from '$lib/components/Timeline.svelte';
 	import TimelineItem from '$lib/components/TimelineItem.svelte';
+	import KeywordHighlight from '$lib/components/KeywordHighlight.svelte';
+
+	import {timelines} from '$lib/data/natta-timeline.json';
 </script>
 
 <svelte:head>
@@ -9,7 +12,7 @@
 </svelte:head>
 
 <section
-	class="max-w-4xl p-6 mx-auto mt-16 mb-4 bg-sky-200 sm:rounded-md shadow-md dark:bg-sky-700 relative"
+	class="max-w-4xl p-6 mx-auto my-4 bg-sky-200 sm:rounded-md shadow-md dark:bg-sky-700 relative"
 >
 	<div class="flex flex-row gap-3 justify-items-center">
 		<div class="basis-1/5 grow-0">
@@ -19,19 +22,11 @@
 			<h2 class="h2 font-mono mb-4">Hello, I'm Natta</h2>
 			<p class="lg:indent-8 indent-0">
 				I'm a Java Back-End developer, experience in
-				<b
-					class="underline underline-offset-4 decoration-pink-600 dark:decoration-pink-300 decoration-wavy decoration-2"
-				>
-					Spring Boot
-				</b>, design & develop Restful API.
+				<KeywordHighlight>Spring Boot</KeywordHighlight>, design & develop Restful API.
 			</p>
 			<p class="lg:indent-8 indent-0">
 				Develop this site with inspiration to learn develop Front-End with
-				<b
-					class="underline underline-offset-4 decoration-pink-600 dark:decoration-pink-300 decoration-wavy decoration-2"
-				>
-					Svelte-Kit
-				</b>.
+				<KeywordHighlight>Svelte-Kit</KeywordHighlight>.
 			</p>
 		</div>
 	</div>
@@ -39,9 +34,12 @@
 <section
 	class="max-w-4xl p-6 mx-auto bg-slate-100 sm:rounded-md shadow-md dark:bg-gray-900 relative"
 >
-	<h1 class="h1 font-sans mb-8">Timeline</h1>
+	<h1 class="h1 font-sans mb-8">Bio Timeline</h1>
 
 	<Timeline>
-		<TimelineItem />
+		{#each timelines as timeline}
+		<TimelineItem {...timeline}
+		icon="mdi:university" />
+		{/each}
 	</Timeline>
 </section>
