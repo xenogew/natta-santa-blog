d41643bcd36d091b5fbfa0439ec680388559953e
WangAphisit Nattasan <w.nattasan@srsys.jp>
2025-05-12 17:52:47 +0900

    recreate project from ground-up

--- tests/about.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test(`about page expected
-    - h1 to contains 'Hello I'm Natta' and
-    - p to contains 'This is about page'`, async ({ page }) => {
-	await page.goto('/about');
-	expect(await page.textContent('h1')).toBe(`Hello I'm Natta`);
-	expect(await page.textContent('p')).toBe(`This is about page`);
-});
--- tests/blog.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test(`blog page expected h1 to contains 'Blog' as title`, async ({ page }) => {
-	await page.goto('/blog');
-	expect(await page.textContent('h1')).toBe(`Blog`);
-});
