869c7760a4fb177d0b41c095e6b5a6107c507b41
Natta Wang <xenogew@gmail.com>
2025-05-12 20:59:32 +0900

    fix theme switcher

--- src/lib/components/ThemeSwitch.svelte
+++ src/lib/components/ThemeSwitch.svelte
@@ -2,14 +2,22 @@
 	import Icon from '@iconify/svelte';
 	import { Switch } from '@skeletonlabs/skeleton-svelte';
 
-	const switchMode = () => {
+	const switchMode = (checked: boolean) => {
+		mode = checked;
 		document.documentElement.classList.toggle('dark');
 	};
 
 	let mode = $state(false);
 </script>
 
-<Switch name="mode" controlActive="bg-surface-200" bind:checked={mode} onCheckedChange={switchMode}>
+<Switch
+	name="mode"
+	controlActive="bg-surface-200"
+	checked={mode}
+	onCheckedChange={(e) => {
+		switchMode(e.checked);
+	}}
+>
 	{#snippet inactiveChild()}<Icon icon="lucide:moon" width="14" height="14" />{/snippet}
 	{#snippet activeChild()}<Icon icon="lucide:sun" width="14" height="14" />{/snippet}
 </Switch>
