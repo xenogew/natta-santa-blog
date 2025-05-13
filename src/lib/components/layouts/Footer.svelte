daf225950602f5a72759f2651e621a7b3dd408ed
Natta Wang <xenogew@gmail.com>
2025-05-13 22:18:25 +0900

    change color of footer to make sure it won't be same as body and header shades anymore

--- src/lib/components/layouts/Footer.svelte
+++ src/lib/components/layouts/Footer.svelte
@@ -2,6 +2,6 @@
 	import BuyMeCoffee from '$lib/components/BuyMeCoffee.svelte';
 </script>
 
-<footer class="bg-surface-300 dark:bg-surface-800 z-100 flex w-full flex-col space-y-4 p-4">
+<footer class="bg-stone-300 dark:bg-stone-900 z-100 flex w-full flex-col space-y-4 p-4">
 	<BuyMeCoffee bmcId="xenogew" bmcText="Buy me a coffee" />
 </footer>
