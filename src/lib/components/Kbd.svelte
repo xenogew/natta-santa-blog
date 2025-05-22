94067a18c2d3dc85a1cc4d7212f90973b1f45d89
Natta Wang <xenogew@gmail.com>
2025-05-22 23:09:26 +0900

    create Kbd.svelte -keyboard key looks UI component

---
+++ src/lib/components/Kbd.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	/**
+	 * Keyboard component for displaying keyboard shortcuts
+	 * Usage:
+	 * <Kbd>Ctrl</Kbd> + <Kbd>C</Kbd>
+	 */
+	let { children } = $props();
+</script>
+
+<kbd
+	class="inline-flex h-5 items-center justify-center rounded border border-gray-200 bg-gray-100 px-2 py-3.5 align-middle font-mono text-xs leading-none font-medium text-gray-800 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
+>
+	{@render children()}
+</kbd>
