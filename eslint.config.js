3e61c7ab3a55e3d00fbfe0282a508439ade1eb30
Natta Wang <xenogew@gmail.com>
2025-05-29 23:15:49 +0900

    change config for formatter and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -18,9 +18,9 @@ export default ts.config(
 	...svelte.configs.prettier,
 	{
 		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
+			globals: { ...globals.browser, ...globals.node },
 		},
-		rules: { 'no-undef': 'off' }
+		rules: { 'no-undef': 'off' },
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
@@ -29,8 +29,13 @@ export default ts.config(
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
-				svelteConfig
-			}
-		}
-	}
+				svelteConfig,
+			},
+		},
+		settings: {
+			svelte: {
+				ignoreWarnings: ['svelte/no-at-html-tags'],
+			},
+		},
+	},
 );
