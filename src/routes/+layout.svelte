6d8a2eedd98dd170b641a5e156d011eb11e154a8
Natta Wang <xenogew@gmail.com>
2025-12-05 23:25:04 +0900

    separate layout structure header-container-footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	import '@fontsource-variable/noto-sans-thai';
+	import '../app.css';
 	import type { LayoutData } from './$types';
 	import { onMount, type Snippet } from 'svelte';
-	import '../app.css';
 	import { fade } from 'svelte/transition';
 	import Header from '$lib/components/layouts/Header.svelte';
 	import Footer from '$lib/components/layouts/Footer.svelte';
@@ -24,24 +25,22 @@
 </svelte:head>
 
 <!-- Main Layout -->
-<section class="preset-soft-surface grid h-screen max-w-full grid-rows-[auto_1fr] overflow-y-auto">
+<main class="preset-soft-surface grid h-screen max-w-full grid-rows-[auto_1fr] overflow-y-auto">
 	<Header />
 
 	<!-- Page Route Content -->
-	<div class="bg-surface-50 dark:bg-surface-800 h-full overflow-y-auto">
-		<main class="flex min-h-full flex-col">
-			<div class="container mx-0 flex-1 md:mx-auto">
-				{#key data.currentRoute}
-					<main
-						class="mx-auto md:mx-6"
-						in:fade={{ duration: 150, delay: 150 }}
-						out:fade={{ duration: 150 }}
-					>
-						{@render children()}
-					</main>
-				{/key}
-			</div>
-			<Footer />
-		</main>
+	<div class="bg-surface-50 dark:bg-surface-800 flex-1 overflow-y-auto">
+		<section class="container mx-auto px-4 py-8">
+			{#key data.currentRoute}
+				<div
+					class="mx-auto md:mx-6"
+					in:fade={{ duration: 150, delay: 150 }}
+					out:fade={{ duration: 150 }}
+				>
+					{@render children()}
+				</div>
+			{/key}
+		</section>
 	</div>
-</section>
+	<Footer />
+</main>
